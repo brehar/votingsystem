fe4ca86f82c0c743741ba8c3df20e196ce6f6108
Brett Hartman <brett.hartman@mac.com>
2019-03-17 15:58:07 -0700

    Create Candidate entity

---
+++ src/main/java/me/bretthartman/votingsystem/entity/Candidate.java
@@ -0,0 +1,20 @@
+package me.bretthartman.votingsystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "candidates")
+public @Data class Candidate {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "citizen_name")
+  private String name;
+}
