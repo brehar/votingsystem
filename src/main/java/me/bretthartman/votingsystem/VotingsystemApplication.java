4b09687c3d2ff737929ca07272989baffe6e893d
Brett Hartman <brett.hartman@mac.com>
2019-03-17 14:59:22 -0700

    Initial commit

---
+++ src/main/java/me/bretthartman/votingsystem/VotingsystemApplication.java
@@ -0,0 +1,13 @@
+package me.bretthartman.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VotingsystemApplication.class, args);
+  }
+
+}
